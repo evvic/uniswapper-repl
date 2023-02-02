1157fde72ff1f83a3832dbc3cb5cd99f0e62e429
Eric Brown <56092687+evvic@users.noreply.github.com>
2023-02-02 14:25:36 +0000

    init uniswap package

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import os
+import sys
+
+METAMASK_WALLET_ADDY = os.environ['METAMASK_WALLET_ADDY']
+
+METAMASK_PRIVATE_KEY = os.environ['METAMASK_PRIVATE_KEY']
+
+PROVIDER = os.environ['INFURA_PROVIDER']
+
+from uniswap import Uniswap
+
+VERSION = 2
+
 print('uniswapper')
+print(METAMASK_WALLET_ADDY, METAMASK_PRIVATE_KEY, PROVIDER)
+
+print(sys.version)
+
+uniswap = Uniswap(address=METAMASK_WALLET_ADDY, private_key=METAMASK_PRIVATE_KEY, version=VERSION, provider=PROVIDER)
+
+
+
+
 
